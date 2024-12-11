be125a6b1b78f5ce31abec72405fcf7a04b9ac39
Apiraam Selvabaskaran <apiraam96@gmail.com>
2024-12-11 16:52:28 -0500

    feat: added basic content for hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="">
+      <h1 className="text-9xl">Plate Designer Tool</h1>
+      <h3>
+        96 well plate designer tool for wet lab scientists, experimentalists,
+        and lab automation experts - use this tool to experimentally design
+        qPCRs, ELISAs, dose response curves, transfections and more!
+      </h3>
+      <button>Try it out!</button>
+      <h2>No more excel sheets</h2>
+      <h3>Keep track of all of your relevent metadata</h3>
+      <h3>Visualize your experimental plan to reduce cognative load</h3>
+      <h3>Export your data to a CSV or JSON to share with others</h3>
+      <h2>About yourself</h2>
+    </div>
+  );
+};
+
+export default Hero;
