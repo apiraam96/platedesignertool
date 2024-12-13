b17e745a0d52adff8e547703b109e81a2bcca346
Apiraam Selvabaskaran <apiraam96@gmail.com>
2024-12-12 20:26:11 -0500

    feat: added column and row labels

--- src/components/Plate.tsx
+++ src/components/Plate.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import Well from "./Well";
 
 const Plate = () => {
-  const cols = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  const rows = ["A", "B", "C", "D", "E", "F", "G", "H"];
+  const cols = ["", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const rows = ["", "A", "B", "C", "D", "E", "F", "G", "H"];
 
   const wellIds = [];
-  for (let i = 0; i < rows.length; i++) {
-    for (let k = 0; k < cols.length; k++) {
+  for (let i = 1; i < rows.length; i++) {
+    for (let k = 1; k < cols.length; k++) {
       wellIds.push(rows[i] + cols[k]);
     }
   }
@@ -16,10 +16,20 @@ const Plate = () => {
 
   return (
     <div className="m-auto col-span-3 p-5">
-      <div className="grid grid-cols-12 border-black border p-16">
-        {wellIds.map((id) => (
-          <Well id={id} />
+      <div className="grid grid-cols-13 grid-rows-9 border-black border p-10 rounded-tl-3xl">
+        {cols.map((column, i) => (
+          <p className={`mx-auto my-auto row-start-1 col-start-${i + 1}`}>
+            {column}
+          </p>
         ))}
+        {rows.map((row, i) => (
+          <p className={`my-auto pr-5 col-start-1 row-start-${i + 1}`}>{row}</p>
+        ))}
+        <div className="grid grid-cols-12 col-span-12 row-span-8 col-start-2 row-start-2">
+          {wellIds.map((id) => (
+            <Well key={id} id={id} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/Well.tsx
+++ src/components/Well.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Well = ({ id }: { id: string }) => {
+const Well = ({ key, id }: { key: string; id: string }) => {
   return (
     <div
       id={id}
+      key={key}
       className="border-slate-800 border p-8 hover:bg-sky-100 hover:cursor-pointer"
     ></div>
   );
